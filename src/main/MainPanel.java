1438a75e59d1187623ca62a0943a5fb959db5ada
James Ding <jamesding223@gmail.com>
2024-12-20 14:15:26 -0500

    Added the title and input labels and text boxes.

---
+++ src/main/MainPanel.java
@@ -0,0 +1,93 @@
+package main;
+
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class MainPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -892554275836568837L; 
+	
+	public MainPanel(Font paramLabelFont, Font inputFont) {
+		
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		
+		
+		JPanel labelPanel = new JPanel();
+		labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
+		
+		JLabel urlLabel = new JLabel("URL Address:");
+		urlLabel.setFont(paramLabelFont);
+		urlLabel.setAlignmentX(RIGHT_ALIGNMENT);
+		JLabel titleLabel = new JLabel("Title:");
+		titleLabel.setFont(paramLabelFont);
+		titleLabel.setAlignmentX(RIGHT_ALIGNMENT);
+		JLabel artistLabel = new JLabel("Artist:");
+		artistLabel.setFont(paramLabelFont);
+		artistLabel.setAlignmentX(RIGHT_ALIGNMENT);
+		JLabel albumLabel = new JLabel("Album:");
+		albumLabel.setFont(paramLabelFont);
+		albumLabel.setAlignmentX(RIGHT_ALIGNMENT);
+		JLabel directoryLabel = new JLabel("Download Location:");
+		directoryLabel.setFont(paramLabelFont);
+		directoryLabel.setAlignmentX(RIGHT_ALIGNMENT);
+		
+		labelPanel.add(urlLabel);
+		labelPanel.add(Box.createVerticalStrut(3));
+		labelPanel.add(titleLabel);
+		labelPanel.add(Box.createVerticalStrut(3));
+		labelPanel.add(artistLabel);
+		labelPanel.add(Box.createVerticalStrut(3));
+		labelPanel.add(albumLabel);
+		labelPanel.add(Box.createVerticalStrut(3));
+		labelPanel.add(directoryLabel);
+		add(labelPanel);
+		add(Box.createHorizontalStrut(5));
+		
+		
+		JPanel textFieldPanel = new JPanel();
+		textFieldPanel.setLayout(new BoxLayout(textFieldPanel, BoxLayout.Y_AXIS));
+		
+		JTextField urlTextField = new JTextField();
+		urlTextField.setFont(inputFont);
+		urlTextField.setColumns(15);
+		urlTextField.setAlignmentX(LEFT_ALIGNMENT);
+		JTextField titleTextField = new JTextField();
+		titleTextField.setFont(inputFont);
+		titleTextField.setColumns(10);
+		titleTextField.setAlignmentX(LEFT_ALIGNMENT);
+		JTextField artistTextField = new JTextField();
+		artistTextField.setFont(inputFont);
+		artistTextField.setColumns(10);
+		artistTextField.setAlignmentX(LEFT_ALIGNMENT);
+		JTextField albumTextField = new JTextField();
+		albumTextField.setFont(inputFont);
+		albumTextField.setColumns(10);
+		albumTextField.setAlignmentX(LEFT_ALIGNMENT);
+		JTextField directoryTextField = new JTextField();
+		directoryTextField.setFont(inputFont);
+		albumTextField.setColumns(17);
+		directoryTextField.setAlignmentX(LEFT_ALIGNMENT);
+		
+		textFieldPanel.add(urlTextField);
+		labelPanel.add(Box.createRigidArea(new Dimension(1, 3)));
+		textFieldPanel.add(titleTextField);
+		labelPanel.add(Box.createRigidArea(new Dimension(1, 3)));
+		textFieldPanel.add(artistTextField);
+		labelPanel.add(Box.createRigidArea(new Dimension(1, 3)));
+		textFieldPanel.add(albumTextField);
+		labelPanel.add(Box.createRigidArea(new Dimension(1, 3)));
+		textFieldPanel.add(directoryTextField);
+		add(textFieldPanel);
+		
+	}
+	
+}
--- src/main/Testing.java
+++ src/main/Testing.java
@@ -1,7 +1,15 @@
 package main;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Testing extends JFrame {
 
@@ -9,13 +17,42 @@ public class Testing extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 8433054944653490532L;
-
-	public Testing() {
+	
+	private static final int WIDTH = 520;
+	private static final int HEIGHT = 399;
+	private static final Font TITLE_FONT = new Font("Segoe UI", Font.BOLD, 45);
+	private static final Font PARAM_LABEL_FONT = new Font("Segoe UI", Font.BOLD, 16);
+	private static final Font INPUT_FONT = new Font("Segoe UI", Font.PLAIN, 12);
+	
+	private Testing() {
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(600, 500);
+		setSize(WIDTH, HEIGHT);
 		setTitle("staccato");
-		setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+		setLayout(new GridBagLayout());
+		setResizable(false);
+		
+		
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+		
+		
+		JPanel titlePanel = new JPanel();
+		titlePanel.setLayout(new BorderLayout());
+		
+		JLabel titleLabel = new JLabel("staccato");
+		titleLabel.setFont(TITLE_FONT);
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		titlePanel.add(titleLabel, BorderLayout.CENTER);
+		contentPanel.add(titlePanel);
+		
+		
+		JPanel mainPanel =  new MainPanel(PARAM_LABEL_FONT, INPUT_FONT);		
+		contentPanel.add(mainPanel);
+		
+		
+		add(contentPanel, new GridBagConstraints());
 		
 	}
 	
